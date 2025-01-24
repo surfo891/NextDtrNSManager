a6513e5686cc161ae6184e2795b4fcd5155884c7
doubleangels <matt@doubleangels.com>
2025-01-24 01:18:29 -0700

    Move function to a lambda

--- app/src/main/java/com/doubleangels/nextdnsmanagement/sentry/SentryInitializer.java
+++ app/src/main/java/com/doubleangels/nextdnsmanagement/sentry/SentryInitializer.java
@@ -9,21 +9,19 @@ import io.sentry.android.core.SentryAndroid;
 public class SentryInitializer {
 
     public static void initialize(Context context) {
-        new Thread(() -> {
-            SentryAndroid.init(context, options -> {
-                options.setDsn("https://8b52cc2148b94716a69c9a4f0c0b4513@o244019.ingest.us.sentry.io/6270764");
-                options.setRelease(BuildConfig.VERSION_NAME);
-                options.enableAllAutoBreadcrumbs(true);
-                options.setAttachScreenshot(true);
-                options.setAttachViewHierarchy(true);
-                options.setTracesSampleRate(1.0);
-                options.setEnableAppStartProfiling(true);
-                options.setAnrEnabled(true);
-                options.setCollectAdditionalContext(true);
-                options.setEnableFramesTracking(true);
-                options.setEnableAppStartProfiling(true);
-                options.setEnableRootCheck(true);
-            });
-        }).start();
+        new Thread(() -> SentryAndroid.init(context, options -> {
+            options.setDsn("https://8b52cc2148b94716a69c9a4f0c0b4513@o244019.ingest.us.sentry.io/6270764");
+            options.setRelease(BuildConfig.VERSION_NAME);
+            options.enableAllAutoBreadcrumbs(true);
+            options.setAttachScreenshot(true);
+            options.setAttachViewHierarchy(true);
+            options.setTracesSampleRate(1.0);
+            options.setEnableAppStartProfiling(true);
+            options.setAnrEnabled(true);
+            options.setCollectAdditionalContext(true);
+            options.setEnableFramesTracking(true);
+            options.setEnableAppStartProfiling(true);
+            options.setEnableRootCheck(true);
+        })).start();
     }
 }
